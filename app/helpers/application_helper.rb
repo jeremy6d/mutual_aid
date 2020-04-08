eb2fa7340e7f85dbdc79cfae0fa296ee2446aa56
Jeremy Weiland <jeremy6d@gmail.com>
2020-04-07 23:25:52 -0400

    google voice support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def link_to_google_voice(number)
+    full_number = "1#{number}" unless number.to_s[0] == "1"
+    link_to "https://voice.google.com/u/0/calls?a=nc,%2B#{full_number}" do
+      fa_icon "phone", base: "google", text: number_to_phone(number, area_code: true)
+    end
+  end
 end
