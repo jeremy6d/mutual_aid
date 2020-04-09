24a68641461885fabd947e41eb6248e74b7be46a
Jeremy Weiland <jeremy6d@gmail.com>
2020-04-08 20:45:45 -0400

    fix problem in migration

--- db/migrate/20200408194004_add_approved_and_phone_number_to_volunteer.rb
+++ db/migrate/20200408194004_add_approved_and_phone_number_to_volunteer.rb
@@ -8,16 +8,22 @@ class Volunteer
                            class_name: "Volunteer"
 end
 
-APPROVER = Volunteer.first
-
 class AddApprovedAndPhoneNumberToVolunteer < ActiveRecord::Migration[6.0]
- def change
+ def up
     add_reference :volunteers, :approved_by, foreign_key: { to_table: :volunteers }
     add_column :volunteers, :phone_number, :string
 
     Volunteer.all.each do |v|
-      v.approved_by = APPROVER
-      puts "#{v.full_name} approved by #{APPROVER.full_name}"
+      v.approved_by = Volunteer.first
+      v.save
+      v2 = Volunteer.find(v.id)
+      puts "#{v2.full_name} approved by #{v2.approved_by&.full_name || 'EMPTY'}"
     end
+    puts "Volunteers still unapproved: #{Volunteer.where(approved_by_id: nil).count}"
+  end
+
+  def down
+    remove_reference :volunteers, :approved_by
+    remove_column :volunteers, :phone_number
   end
 end
