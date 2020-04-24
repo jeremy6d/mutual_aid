99d3b8cf8588a0602f975dc983dfecaf4e7b158d
Jeremy Weiland <jeremy6d@gmail.com>
2020-04-24 14:17:25 -0400

    badges for deliveries, and show driver

--- app/helpers/deliveries_helper.rb
+++ app/helpers/deliveries_helper.rb
@@ -1,2 +1,33 @@
 module DeliveriesHelper
+  def delivery_badge_for(delivery)
+    content_tag :span, class: "badge badge-#{status_color_for(delivery)}" do
+      fa_icon status_icon_for(delivery), text: delivery.status.titleize
+    end
+  end
+
+  def delivery_color_for(delivery)
+    case delivery.status
+    when Delivery::Status::EMPTY
+      'danger'
+    when Delivery::Status::ON_THE_WAY
+      'warning'
+    when Delivery::Status::CANCELLED
+      'dark'
+    when Delivery::Status::DELIVERED
+      'success'
+    end
+  end
+
+  def delivery_icon_for(delivery)
+    case delivery.status
+    when Delivery::Status::EMPTY
+      'box-open'
+    when Delivery::Status::ON_THE_WAY
+      'shipping-fast'
+    when Delivery::Status::CANCELLED
+      'ban'
+    when Delivery::Status::DELIVERED
+      'clipboard-check'
+    end
+  end
 end
