4a7d44cff116197b202d3ebc63daf8f060519371
Jeremy Weiland <jeremy6d@gmail.com>
2020-06-25 15:02:44 -0400

    clean up specs and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "--- Volunteers ---"
 if Volunteer.count.zero?
   admin = Volunteer.create! first_name: "Jeremy", 
                             last_name: "Weiland", 
@@ -13,9 +14,17 @@ if Volunteer.count.zero?
                             password_confirmation: "password",
                             email: "jeremy@jeremyweiland.com"
   admin.approved_by = admin
+  puts "\t* Created #{admin.full_name}"
   admin.save
 end
 
-20.times do 
-  FactoryBot.create :random_aid_request
+puts "--- Aid Requests ---"
+20.times do |n|
+  time = (20 - n).days.ago
+  Timecop.travel(time) do
+    ar = FactoryBot.create :random_aid_request
+    puts "\t* Created aid request: #{ar.attributes.to_s}"
+  end
 end
+
+puts "--- COMPLETE ---"
